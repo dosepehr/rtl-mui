46704fadc332267770ed6fdc85c0cd8b56c95832
dosepehr <dosepehr@gmail.com>
2023-02-09 02:03:25 +0330

    creating layouts

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import { ThemeProvider} from '@mui/material/styles';
-import rtlPlugin from 'stylis-plugin-rtl';
-import { CacheProvider } from '@emotion/react';
-import { HelmetProvider, Helmet } from 'react-helmet-async';
-import createCache from '@emotion/cache';
-import { prefixer } from 'stylis';
-import {theme} from './components/ui/theme';
 import Header from './components/ui/Header';
-
-
-//NOTE Create RTL Cache
-const cacheRTL = createCache({
-    key: 'muirtl',
-    stylisPlugins: [prefixer, rtlPlugin],
-});
-
+import MainLayout from './components/layouts/MainLayout';
 function App() {
     return (
-        <CacheProvider value={cacheRTL}>
-            <ThemeProvider theme={theme}>
-                <HelmetProvider>
-                    <Helmet>
-                        <title>وب سایت شخصی یونس قربانی</title>
-                    </Helmet>
-                    <Header />
-                </HelmetProvider>
-            </ThemeProvider>
-        </CacheProvider>
+        <>
+            <MainLayout>
+                <Header />
+            </MainLayout>
+        </>
     );
 }
 
